1504fc3d867fe860241247da635572730e3fe463
Stas <mccleric@ya.ru>
2023-01-13 16:47:17 +0300

    Добавление jwt. проблема с совместимостью.

--- Initializr/src/main/java/com/voronkov/Initializr/config/SecurityConfig.java
+++ Initializr/src/main/java/com/voronkov/Initializr/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,7 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Slf4j
-@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true,jsr250Enabled = true)
 //@RequestMapping("api/v1/products")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserServise userServise;
---
+++ Initializr/src/main/java/com/voronkov/Initializr/dto/AuthResponse.java
@@ -0,0 +1,9 @@
+package com.voronkov.Initializr.dto;
+
+import lombok.Value;
+
+@Value
+public class AuthResponse {
+
+    String token;
+}
